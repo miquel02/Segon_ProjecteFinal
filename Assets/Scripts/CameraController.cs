587778fcc497d61c8248c7ee2d39be0eac84d8a4
miquel02 <mgalmespascual@gmail.com>
2023-01-17 10:42:49 +0100

    .

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,22 +5,39 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
-    public GameObject cameraObjective;
+    public GameObject[] cameraObjective;
+
+    public int cameraTarget;
+    public bool camOnPlayer;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameraTarget = 0;
+        camOnPlayer = true;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        if (Input.GetKey(KeyCode.Q) && camOnPlayer)
+        {
+                cameraTarget++; 
+                camOnPlayer = false;
+            
+        }
+        if (Input.GetKey(KeyCode.Q) && !camOnPlayer)
+        {
+            cameraTarget = 0;
+            camOnPlayer = true;
+        }
+
         //Vector3 pos = cameraObjective.transform.position;
         //pos.z += cameraHeight;
         //transform.position = pos;
-        transform.position = Vector3.MoveTowards(transform.position, cameraObjective.transform.position, 0.03f);
+        transform.position = Vector3.MoveTowards(transform.position, cameraObjective[cameraTarget].transform.position, 0.03f);
     }
 }
